f0bf321241765a5c475f84475c3d4e72983c8a67
Nilesh Patel <61546940+NileshPPatel@users.noreply.github.com>
2020-04-15 13:08:36 +0200

    feat: add print statements to display progress and results of functions

--- etl.py
+++ etl.py
@@ -9,11 +9,18 @@ def load_staging_tables(cur, conn):
       
     INPUTS:
     * cur the cursor variable
-    * conn the database connection 
+    * conn the database connection
     """
+    print("Loading data from S3 to AWS Reshift tables.")
     for query in copy_table_queries:
+        print('------------------')
+        print('Processing query: {}'.format(query))
         cur.execute(query)
         conn.commit()
+        print('------------------')
+        print('{} processed OK.'.format(query))
+        
+    print('All data loaded from S3 to Redshift.')
 
 
 def insert_tables(cur, conn):
@@ -24,17 +31,24 @@ def insert_tables(cur, conn):
     * cur the cursor variable
     * conn the database connection 
     """
+    print("Loading data from staging tables to analytics tables.")
     for query in insert_table_queries:
+        print('------------------')
+        print('Processing query: {}'.format(query))
         cur.execute(query)
         conn.commit()
+        print('------------------')
+        print('{} processed OK.'.format(query))
 
+    print('All data loaded from staging tables to analytics tables.')
 
 def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
-    cur = conn.cursor()
+    cur = conn.cursor() 
+    print("AWS Redshift connection established OK.")
     
     load_staging_tables(cur, conn)
     insert_tables(cur, conn)
